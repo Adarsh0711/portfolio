3254fdb3e4c82d050144ae090699104a5d71ad4b
Adarshro1 <adarshreddybandaru123@gmail.com>
2024-07-30 21:54:30 -0400

    ch6

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -4,6 +4,7 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { CiMenuFries } from "react-icons/ci";
+import { useState } from "react";
 
 const links = [
   {
@@ -29,37 +30,46 @@ const links = [
 ];
 
 const MobileNav = () => {
+  const [navOpen, setNavOpen] = useState(false);
   const pathname = usePathname();
+
+  const toggleNav = () => {
+    setNavOpen(!navOpen);
+  };
+
+  const closeNav = () => {
+    setNavOpen(false);
+  };
+
   return (
-    <Sheet>
-      <SheetTrigger className="flex justify-center items-center">
+    <Sheet open={navOpen} onOpenChange={toggleNav}>
+      <SheetTrigger className="flex justify-center items-center" onClick={toggleNav}>
         <CiMenuFries className="text-[32px] text-accent" />
       </SheetTrigger>
       <SheetContent className="flex flex-col">
         {/* logo */}
         <div className="mt-32 mb-40 text-center text-2xl">
           <Link href="/">
-            <h1 className="text-4xl font-semibold">
+            <h1 className="text-4xl font-semibold" onClick={closeNav}>
               AB<span className="text-accent">.</span>
             </h1>
           </Link>
         </div>
         {/* nav */}
         <nav className="flex flex-col justify-center items-center gap-8">
-          {links.map((link, index) => {
-            return (
-              <Link
-                href={link.path}
-                key={index}
+          {links.map((link, index) => (
+            <Link href={link.path} key={index} passHref>
+              <a
                 className={`${
                   link.path === pathname &&
                   "text-accent border-b-2 border-accent"
                 } text-xl capitalize hover:text-accent transition-all`}
+                onClick={closeNav}
               >
                 {link.name}
-              </Link>
-            );
-          })}
+              </a>
+            </Link>
+          ))}
         </nav>
       </SheetContent>
     </Sheet>
